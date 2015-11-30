34e0de73f58916f3d3bffdcf02d2399abcdf1a08
Joshua Kifer <joshua.kifer@gmail.com>
2015-11-30 13:07:04 -0800

    Added connection pool

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var Connection = require('./lib/connection.js');
+var Pool = require('./lib/pool.js');
 var mysql = require('mysql');
 
 exports.createConnection = function(config){
     return new Connection(config);
+}
+
+exports.createPool = function(config){
+    return new Pool(config);
 }
 
 exports.Types = mysql.Types;
